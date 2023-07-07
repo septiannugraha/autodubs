30ec4d9f3283e893ef1a1c2025632a662b05b6ae
Septian Adi Nugraha <septian.nugraha@bpkp.go.id>
2023-07-07 15:12:51 +0700

    latest update

--- elevenlabs-dubber.py
+++ elevenlabs-dubber.py
@@ -3,19 +3,12 @@ import whisper
 from pytube import YouTube
 from pydub import AudioSegment
 import pandas as pd
+import anthropic
 import io
+from elevenlabs import generate, set_api_key
 import os
 import subprocess
-import anthropic
-from elevenlabs import generate, set_api_key
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
-
-def combine_video(video_filename):
-    ffmpeg_extract_subclip(video_filename, 0, 60, targetname="cut_video.mp4")
-    output_filename = "output.mp4"
-    command = ["ffmpeg", "-y", "-i", "cut_video.mp4", "-i", "output.mp3", "-c:v", "copy", "-c:a", "aac", output_filename]
-    subprocess.run(command)
-    return output_filename
 
 def shorten_audio(filename):
     filename = "cut_audio.mp4"
@@ -41,8 +34,7 @@ def generate_translation(original_text, destination_language):
     return resp.completion
 
 def generate_dubs(text):
-    # main_audio = AudioSegment.silent(duration=int(60 * 1000))
-
+    filename = "output.mp3"
     set_api_key(st.secrets['xi_api_key'])
     audio = generate(
                     text=text,
@@ -52,26 +44,17 @@ def generate_dubs(text):
 
     audio_io = io.BytesIO(audio)
     insert_audio = AudioSegment.from_file(audio_io, format='mp3')
-    # for i, row in df.iterrows():
-    #     set_api_key(st.secrets['xi_api_key'])
-    #     audio = generate(
-    #                     text=row['text'],
-    #                     voice="Bella",
-    #                     model='eleven_monolingual_v1'
-    #                 )
+    insert_audio.export(filename, format="mp3")
+    return filename
 
-    #     audio_io = io.BytesIO(audio)
+def combine_video(video_filename, audio_filename):
+    ffmpeg_extract_subclip(video_filename, 0, 60, targetname="cut_video.mp4")
+    output_filename = "output.mp4"
+    command = ["ffmpeg", "-y", "-i", "cut_video.mp4", "-i", audio_filename, "-c:v", "copy", "-c:a", "aac", output_filename]
+    subprocess.run(command)
+    return output_filename
 
-    #                 # Load the TTS audio
-    #     insert_audio = AudioSegment.from_file(audio_io, format='mp3')
 
-    #                 # Calculate the start and end times in milliseconds
-    #     start_time = int(row['start'] * 1000)
-    #     end_time = int(row['end'] * 1000)
-
-    #                 # Insert the TTS audio into the main audio
-    #     main_audio = main_audio[:start_time] + insert_audio + main_audio[end_time:]
-    return insert_audio
 st.title("AutoDubs 📺🎵")
 
 link = st.text_input("Link to Youtube Video", key="link")
@@ -99,19 +82,20 @@ if st.button("Transcribe!"):
                 df = pd.DataFrame(transcription['segments'], columns=['start', 'end', 'text'])
                 st.dataframe(df)
 
-                dubbing_caption = st.caption("Begin dubbing...")
-                # main_audio = generate_dubs(df)
-                translation = generate_translation(transcription['text'], language)
-                main_audio = generate_dubs(translation)
+                dubbing_caption = st.caption("Generating translation...")
 
-                main_audio.export("output.mp3", format="mp3")
+                translation = generate_translation(transcription['text'], language)
+
+                dubbing_caption = st.caption("Begin dubbing...")
+                dubs_audio = generate_dubs(translation)
                 dubbing_caption.caption("Dubs generated! combining with the video...")
+
                 video_streams = yt.streams.filter(only_video=True)
                 video_filename = video_streams.first().download()
 
                 if video_filename:
                     dubbing_caption.caption("Video downloaded! combining the video and the dubs...")
-                    output_filename = combine_video(video_filename)
+                    output_filename = combine_video(video_filename, dubs_audio)
                     if os.path.exists(output_filename):
                         dubbing_caption.caption("Video successfully dubbed! Enjoy! 😀")
                         st.video(output_filename)
